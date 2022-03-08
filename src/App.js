cb13c49b2164a0441045f5e803ce73d1ae977726
Tom Bloor <tom@tbsliver.me>
2022-03-08 00:09:14 +0000

    build main stopwatch and tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {Button} from "./components/button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [startTime, setStartTime] = useState(0);
+    const [endTime, setEndTime] = useState(0);
+
+    // time elapsed not including run time
+    const [elapsedTime, setElapsedTime] = useState(0);
+    // time between start/stop pressed
+    const [runTime, setRunTime] = useState(0);
+    // time to display
+    const [displayTime, setDisplayTime] = useState(0);
+
+    const handleStartStop = () => {
+        if (startTime > 0) {
+            // timer is running
+            if (endTime > 0) {
+                // timer has been restarted without reset
+                // add up how much has elapsed then start running again
+                setElapsedTime(prev => prev + runTime);
+                setRunTime(0);
+                setStartTime(new Date().getTime());
+                setEndTime(0);
+            } else {
+                // timer has been stopped
+                setEndTime(new Date().getTime());
+            }
+        } else {
+            // timer has not been started at all, start it
+            setStartTime(new Date().getTime());
+        }
+    }
+
+    const handleReset = () => {
+        setStartTime(0);
+        setEndTime(0);
+        setElapsedTime(0);
+        setRunTime(0);
+    }
+
+    useEffect(() => {
+        const displayTimer = setInterval(() => {
+            if (endTime > 0) {
+                // timer has been started then stopped
+                setRunTime(endTime - startTime);
+            } else if (startTime > 0) {
+                // timer has been started
+                const now = new Date().getTime();
+                setRunTime(now - startTime);
+            }
+            setDisplayTime(runTime + elapsedTime);
+            // 100ms is display update time, actual stopwatch is as accurate as your browser lets you
+        }, 100);
+        return () => clearInterval(displayTimer);
+    }, [startTime, endTime, runTime, elapsedTime]);
+
+    return (
+        <>
+            <h1 id={"stopwatch-elapsed"}>{displayTime}</h1>
+            <Button id={"stopwatch-start-stop"}
+                    onClick={handleStartStop}>{startTime > 0 && endTime === 0 ? "Stop" : "Start"}</Button>
+            <Button id={"stopwatch-reset"} onClick={handleReset}>Reset</Button>
+            {/*<div>Start Time: {startTime}</div>*/}
+            {/*<div>End Time: {endTime}</div>*/}
+            {/*<div>Elapsed Time: {elapsedTime}</div>*/}
+            {/*<div>Run Time: {runTime}</div>*/}
+        </>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,69 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/**
+ * note tests use getByText exact so the tests still work with debug data shown
+ */
+
+test('has start button on init', () => {
+    render(<App/>);
+    const button = screen.getByText("Start", {exact: true});
+    expect(button).toBeInTheDocument();
+    expect(button.id).toEqual('stopwatch-start-stop');
 });
+
+test('start button has correct id', () => {
+    render(<App/>);
+    const button = screen.getByText("Start", {exact: true});
+    expect(button.id).toEqual('stopwatch-start-stop');
+});
+
+test('start button says stop when running', () => {
+    render(<App/>);
+    const button = screen.getByText("Start", {exact: true});
+    expect(button.textContent).toEqual('Start');
+    userEvent.click(button);
+    expect(button.textContent).toEqual('Stop');
+});
+
+test('has reset button on init', () => {
+    render(<App/>);
+    const button = screen.getByText("Reset", {exact: true});
+    expect(button).toBeInTheDocument();
+});
+
+test('reset button has correct id', () => {
+    render(<App/>);
+    const button = screen.getByText("Reset", {exact: true});
+    expect(button.id).toEqual('stopwatch-reset');
+});
+
+test('display time has correct id', () => {
+    render(<App/>);
+    const display = screen.getByRole('heading', {level: 1});
+    expect(display.id).toEqual("stopwatch-elapsed");
+});
+
+test('start time is zero on init', () => {
+    render(<App/>);
+    const display = screen.getByRole('heading', {level: 1});
+    expect(Number(display.textContent)).toEqual(0);
+});
+
+/*
+ This test keeps exploding with issues without heavily diving how timing
+ works with react and jest and testing-library... look later
+
+test('time elapses when started', async () => {
+    render(<App/>);
+    const display = screen.getByRole('heading', {level: 1});
+    expect(display.textContent).toEqual("0");
+
+    userEvent.click(screen.getByText("Start", {exact: true}));
+
+    userEvent.click(screen.getByText("Stop", {exact: true}));
+
+    expect(Number(display.textContent)).toBeGreaterThan(0);
+});
+ */
